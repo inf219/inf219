8ea952c24e8bb7424bf6b5a9a53cd9a3b5464f9f
Emilie <97628822+Emiliham@users.noreply.github.com>
2025-09-21 17:14:21 +0200

    brukt layouten Synne lagde til agent-siden også

---
+++ src/app/agent/layout.tsx
@@ -0,0 +1,43 @@
+// layout.tsx
+import { auth } from "@/auth";
+import HamburgerMenu from "../components/hamburgerMenu";
+import LinkButton from "../components/linkButton";
+
+const backgroundColor = "#C9EDF7";
+const topColor = "#9ADBE8";
+
+export default async function Layout({
+    children,
+    }: {
+    children: React.ReactNode;
+    }) {
+    const session = await auth();
+
+    return (
+        <div className="min-h-screen w-full" style={{ backgroundColor }}>
+        {/* TopBar */}
+        <div
+            className="sticky top-0 z-50 w-full shadow-2xl px-6 py-4 flex items-center justify-between"
+            style={{ backgroundColor: topColor, borderColor: "#6ECAD9" }}
+        >
+            <LinkButton
+                href="/home/student"
+                className="flex items-center space-x-2"
+                >
+                <img
+                    src="/sidestilt-logo-vlfk.png"
+                    alt="Vestland fylkeskommune logo"
+                    className="h-16 w-auto"
+                />
+            </LinkButton>
+            <HamburgerMenu />
+        </div>
+
+        {/* Her får du innhold fra page.tsx */}
+        <main className="flex flex-col items-center justify-between p-8 h-full">
+            {children}
+        </main>
+        </div>
+    );
+}
+
