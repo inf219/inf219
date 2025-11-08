17308f0e3f9aad6b53439c1dc645499f215d47e2
Synne Holen <synnesholen@gmail.com>
2025-11-08 17:51:15 +0100

    Ryddet en del i frontend-koden

--- src/app/components/inputBox.tsx
+++ src/app/components/inputBox.tsx
@@ -28,7 +28,9 @@ export default function InputBox({
             className={`border border-[var(--border_color)] rounded-lg mb-4 
                 bg-[#F0F9FB] px-2 py-2 w-full 
                 ring-1 ring-white/5
-                text-[var(--loading_color)] placeholder:text-slate-500 hover:bg-white hover:shadow-[0_0_40px_rgba(77,168,190,0.35)] shadow-[0_0_8px_rgba(34,211,238,0.3)] ${sizeClasses[size]}`}>
+                text-[var(--loading_color)] placeholder:text-slate-500 
+                hover:bg-white hover:shadow-[0_0_40px_rgba(77,168,190,0.35)] 
+                shadow-[0_0_8px_rgba(34,211,238,0.3)] ${sizeClasses[size]}`}>
         </textarea>
     )
 }
